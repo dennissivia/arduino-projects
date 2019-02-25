2dada43d4f11f8187caef11a93e3485c54cc610a
Dennis Sivia <dennis.sivia@gmail.com>
2019-02-25 20:39:13 +0100

    Adds NFC/RFID support via MFRC522
    Adds means for reading a rfid tag and comparing it to a static
    access tag id. The authorized tag-id is defined in config.h and
    not part of the commit. Just define
    `byte goodTagID[5] = {0x1,0x1,0x1,0x1}` in your config.h with your own
    tag id values and everything should work as expected.

--- door-lock/door-lock.ino
+++ door-lock/door-lock.ino
@@ -1,5 +1,19 @@
 #include <Keypad.h>
+#include <EEPROM.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Wire.h>
+
 #include "config.h"
+#define DEBUG 1
+
+#define RELAY_PIN A5 
+#define BUZZER_PIN A4
+
+#define RST_PIN 9
+#define SS_PIN 10
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+byte tagID[5]; // input buffer for the tag serial
 
 
 const byte numRows= 4; //number of rows on the keypad
@@ -22,35 +36,37 @@ byte colPins[numCols] = {6,7,8,9}; //Columns 0 to 3
 //initializes an instance of the Keypad class
 Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
 
-
-#define RELAY_PIN 12
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); // Initialize serial communications with the PC
-  Serial.println("Started");
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Started");
 }
 
 void loop() {
+  readKeypad();
+  if(checkInputBuffer()){
+    handleCodeValid();
+    resetInputBuffer();
+  }
+  if(validateNFC()){
+    handleTagValid();
+  }
+}
+
+void readKeypad(){
   // put your main code here, to run repeatedly:
   char c=myKeypad.getKey();
   if(c != NO_KEY){
     Serial.println("Key is: " + String(c));
     handleKeyInput(c);
   }
-  if(checkInputBuffer()){
-    handleCodeValid();
-    resetInputBuffer();
-  }else{
-    
-  }
-
 }
 
 void handleKeyInput(const char c){
-
   if(c == 'C'){
     resetInputBuffer();         
   }else{
@@ -80,22 +96,78 @@ void resetInputBuffer(){
   inputBufferPos = 0;
 }
 
-void indicateCorrectKey(){
- 
-}
+void indicateCorrectKey(){}
 void indicateIncorrectKey(){}
 
 const bool checkInputBuffer(){
   return (inputBufferPos == ACCESS_CODE.length() && inputBuffer == ACCESS_CODE);
 }
-void handleCodeValid(){
-  Serial.println("correct code detected");
+
+void unlock(){
+  tone(BUZZER_PIN, 4000, 100);
+  delay(200);
+  tone(BUZZER_PIN, 4000, 200);
+  // noTone(BUZZER_PIN);
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(RELAY_PIN, HIGH);
   delay(2000);
   digitalWrite(RELAY_PIN, LOW);
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-
 }
 
+void handleTagValid(){
+  Serial.println("NFC/RFID tag recognized.");
+  unlock();
+}
+void handleCodeValid(){
+  Serial.println("correct code detected");
+  unlock();
+}
 
+boolean readTagID() {
+  // Getting ready for Reading PICCs
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { //If a new PICC placed to RFID reader continue
+    // Serial.println("no card present");
+    return false;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Since a PICC placed get Serial and continue
+    Serial.println("cannot read card.");
+    return false;
+  }
+  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
+  // I think we should assume every PICC as they have 4 byte UID
+  // Until we support 7 byte PICCs
+  Serial.println(F("Scanned PICC's UID:"));
+  for ( uint8_t i = 0; i < 4; i++) {  //
+    tagID[i] = mfrc522.uid.uidByte[i];
+  }
+  printTagID(tagID);
+  printTagID(goodTagID);
+  mfrc522.PICC_HaltA(); // Stop reading
+  return true;
+}
+
+boolean validateNFC(){
+  if(readTagID()){
+    Serial.println("comparing goodTag with tagID");
+
+    for ( uint8_t i = 0; i < 4; i++) {
+      if(goodTagID[i] != tagID[i]){
+        Serial.println("Sorry. Card not recognized.");
+        return false;
+      }
+    }
+
+    Serial.println("Matching Card detected.");
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void printTagID(byte *tag){
+  for ( uint8_t i = 0; i < 4; i++) {  //
+    Serial.print(tag[i], HEX);
+  }
+  Serial.println("");
+}
